814034df7bd9dff9ced8c2f934e4a66c0aff8785
Thomas-R0 <applepiecustardcream@gmail.com>
2025-08-11 16:46:04 +1000

    Basic example UI block-out

--- UI.py
+++ UI.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.resizable(False, False)
+root.title("Discovr UI")
 
-c = tk.Canvas(root,width=700, height=700)
-c.pack()
+#basic ui shape blocked out with frames | total dimentions are 1000x700
+topbar = tk.Frame(root,width=1000, height=50, background="yellow")
+leftmenu = tk.Frame(root,width=200, height=650, background="red")
+mainblock = tk.Frame(root,width=800, height=550, background="blue")
+submenu = tk.Frame(root,width=800, height=100, background="green")
+
+#place frames in window
+topbar.grid(row= 0, column= 0, columnspan=2)
+leftmenu.grid(row= 1, column= 0, rowspan=2)
+mainblock.grid(row= 2, column= 1)
+submenu.grid(row= 1, column= 1)
+
+
 
 root.mainloop()
