a7b3fc78bc1e510ddd30abaed045ef00563542ad
Thomas-R0 <applepiecustardcream@gmail.com>
2025-08-25 14:03:59 +1000

    Update cloud-scanner.py

--- cloud-scanner.py
+++ cloud-scanner.py
@@ -1,1 +1,14 @@
-print("hello")
+import boto3
+
+boto3
+
+ACCESS_KEY = -1
+SECRET_KEY = -1
+SESSION_TOKEN = -1
+
+client = boto3.client(
+    's3',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+    aws_session_token=SESSION_TOKEN
+)
