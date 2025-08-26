7a291e820686732f4226b0a7e19dfac95ea98eb9
Thomas-R0 <applepiecustardcream@gmail.com>
2025-08-26 15:29:25 +1000

    Update cloud-scanner.py

--- cloud-scanner.py
+++ cloud-scanner.py
@@ -5,15 +5,34 @@ def aws_ec2_scan(): #must have aws cli configured, add option to manually input 
     client = boto3.client('ec2')
 
     results = client.describe_instances()
-    inst = results['Reservations'][0]['Instances'][0]
+
+    for reservation in results['Reservations']:
+        for inst in reservation['Instances']:
+
+            print(inst['InstanceType'])
+            print(inst['State']['Name'])
+            print(inst['InstanceId'])        
+
+            print(inst['PlatformDetails'] + " " + inst['Architecture'])
+            print(inst['Tags'])
+
+            print(inst['PrivateDnsName'])
+            print(inst['PublicDnsName'])
+            print(inst['SecurityGroups'])
+            print(inst['SubnetId'])
+            print(inst['VpcId'])
+
+            print(inst['LaunchTime'])
+
+            print('Private: ' + inst['PrivateIpAddress'])
+            try:
+                print('Public: ' + inst['PublicIpAddress'])
+            except KeyError:
+                print('Public: ' + 'N.A.')
+
+            print("")
+        print(len(results['Reservations']))
 
 
-    print(inst['InstanceType'])
-    print(inst['PlatformDetails'])
-    print(inst['State']['Name'])
-
-
-    print('Private: ' + inst['PrivateIpAddress'])
-    #print('Public: ' + inst['PublicIpAddress'])
 
 aws_ec2_scan()
