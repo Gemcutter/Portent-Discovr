f532beda247fd0930737a4c776b291ee6febf460
Thomas-R0 <applepiecustardcream@gmail.com>
2025-08-27 10:31:24 +1000

    Update cloudScanner.py

--- cloudScanner.py
+++ cloudScanner.py
@@ -7,7 +7,7 @@ def nice_disp(inst, var):
 def aws_ec2_scan(add_log, args): #must have aws cli configured, add option to manually input access and secret keys
     client = boto3.client('ec2')
 
-    results = client.describe_instances() # chang
+    results = client.describe_instances()
 
 
     for reservation in results['Reservations']:
