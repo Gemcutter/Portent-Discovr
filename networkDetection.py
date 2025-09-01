321523ab737c8b47ec4c93da752ca14fc0338498
CMoews <ckmoews@gmail.com>
2025-09-01 14:36:03 +1000

    Added Subnet scanning
    network detection now includes its own subnet scanning.

--- networkDetection.py
+++ networkDetection.py
@@ -1,17 +1,16 @@
 import ipaddress
 import socket
 from scapy.all import ARP, Ether, srp
-
+import subprocess
+import re
 
 def convertSubnetmaskToCidr(subnet_mask):
     #Converts a subnet mask string (e.g., "255.255.255.0") to its CIDR value.
     octets = subnet_mask.split('.')
     binary_string = ""
     for octet in octets:
-        # Convert each octet to its 8-bit binary representation
         binary_string += bin(int(octet))[2:].zfill(8)
     
-    # Count the number of '1's to get the CIDR value
     cidr = binary_string.count('1')
     return cidr
 
@@ -24,7 +23,14 @@ def getNetworkRange(cidr):
 hostname = socket.gethostname()
 address = socket.gethostbyname(hostname)
 
-subnetMask = "255.255.128.0"  # Uni subnet mask = "255.255.128.0", need to find a way to get this dynamically. Worst case, can get client to just run ipconfig and enter it in manually
+#get subnet mask
+output = subprocess.check_output("ipconfig", text=True)
+match = re.search(r"Subnet Mask.*?:\s*([\d.]+)", output)
+if match:
+    subnetMask = match.group(1)
+else:
+    subnetMask = "255.255.255.0" # default to basic subnet mask if not found
+
 cidr = str(convertSubnetmaskToCidr(subnetMask))
 FullAddress = address +'/'+ cidr
 print(FullAddress)
@@ -32,5 +38,6 @@ print(FullAddress)
 # display network range
 start, end = getNetworkRange(FullAddress)
 print(f"Network: {FullAddress}")
+print(f"Subnet mask: {subnetMask}")
 print(f"Start IP: {start}")
 print(f"End IP:   {end}")
