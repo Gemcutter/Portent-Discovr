b9e32e567176a32a1f57aa84a3bfe6645efaa085
Gemcutter <spud.hjc@gmail.com>
2025-10-06 16:37:45 +1100

    Update cloudScanner.py

--- cloudScanner.py
+++ cloudScanner.py
@@ -48,67 +48,73 @@ def nice_disp(inst, var):
     return f'{var}: {inst[var]}'
 
 def aws_ec2_scan(add_log, activeScanning, netMap, args): #must have aws cli configured, add option to manually input access and secret keys
-    if args["use_env"] == 1: # use env variables
-        client = boto3.client('ec2')
+    try:
+        if args["use_env"] == 1: # use env variables
+            client = boto3.client('ec2')
 
-    else: # use entered variables
-        client = boto3.client('ec2',
-                              aws_access_key_id=args["access_key"],
-                              aws_secret_access_key=args["secret_key"],
-                              region_name="ap-southeast-2") # temporary fix
+        else: # use entered variables
+            client = boto3.client('ec2',
+                                aws_access_key_id=args["access_key"],
+                                aws_secret_access_key=args["secret_key"],
+                                region_name="ap-southeast-2") # temporary fix
 
-    
+        
 
-    results = client.describe_instances()
+        results = client.describe_instances()
 
 
-    for reservation in results['Reservations']:
-        add_log("")
-        add_log("===============================")
-        add_log(nice_disp(reservation, 'ReservationId'))
-        add_log(nice_disp(reservation,'OwnerId'))   
-        add_log(nice_disp(reservation,'Groups')) 
-        add_log("-------------------------------")
-
-        for inst in reservation['Instances']:
-            add_log(nice_disp(inst, 'InstanceType'))
-            add_log(nice_disp(inst,'State'))       
-            add_log(nice_disp(inst,'InstanceId'))
-            add_log(nice_disp(inst, 'PlatformDetails'))
-            add_log(nice_disp(inst, 'Architecture'))
-            add_log(nice_disp(inst, 'Tags'))
-            add_log(nice_disp(inst, 'LaunchTime'))
+        for reservation in results['Reservations']:
             add_log("")
+            add_log("===============================")
+            add_log(nice_disp(reservation, 'ReservationId'))
+            add_log(nice_disp(reservation,'OwnerId'))   
+            add_log(nice_disp(reservation,'Groups')) 
+            add_log("-------------------------------")
 
-            add_log(nice_disp(inst, 'PrivateIpAddress'))
-            try:
-                add_log(nice_disp(inst, 'PublicIpAddress'))
-            except KeyError:
-                add_log('Public IP: ' + 'N.A.')
+            for inst in reservation['Instances']:
+                add_log(nice_disp(inst, 'InstanceType'))
+                add_log(nice_disp(inst,'State'))       
+                add_log(nice_disp(inst,'InstanceId'))
+                add_log(nice_disp(inst, 'PlatformDetails'))
+                add_log(nice_disp(inst, 'Architecture'))
+                add_log(nice_disp(inst, 'Tags'))
+                add_log(nice_disp(inst, 'LaunchTime'))
+                add_log("")
 
-            add_log(nice_disp(inst, 'PrivateDnsName'))
-            if inst['PublicDnsName']:
-                add_log(nice_disp(inst, 'PublicDnsName'))
-            else:
-                add_log('PublicDnsName: N.A.')
+                add_log(nice_disp(inst, 'PrivateIpAddress'))
+                try:
+                    add_log(nice_disp(inst, 'PublicIpAddress'))
+                except KeyError:
+                    add_log('Public IP: ' + 'N.A.')
 
-        add_log("-------------------------------")
-        add_log("")
-        activeScanning[0] = False
+                add_log(nice_disp(inst, 'PrivateDnsName'))
+                if inst['PublicDnsName']:
+                    add_log(nice_disp(inst, 'PublicDnsName'))
+                else:
+                    add_log('PublicDnsName: N.A.')
+
+            add_log("-------------------------------")
+            add_log("")
+    except Exception as e:
+        print(e)
+    
+    activeScanning[0] = False
 
 
 def azure_vm_scan(add_log, activeScanning, netMap, args):
-    subscription_id = args["subscription_id"]
+    try:
+        subscription_id = args["subscription_id"]
 
-    credential = DefaultAzureCredential()
-    compute_client = ComputeManagementClient(credential, subscription_id)
+        credential = DefaultAzureCredential()
+        compute_client = ComputeManagementClient(credential, subscription_id)
 
-    # List all VMs in the subscription
-    add_log("===============================")
-    for vm in compute_client.virtual_machines.list_all():
-        add_log(f"Name: {vm.name}")
-        add_log(f"VM Type: {vm.hardware_profile.vm_size}")
-        add_log(f"Location: {vm.location}")
-        add_log("-------------------------------")
-    
+        # List all VMs in the subscription
+        add_log("===============================")
+        for vm in compute_client.virtual_machines.list_all():
+            add_log(f"Name: {vm.name}")
+            add_log(f"VM Type: {vm.hardware_profile.vm_size}")
+            add_log(f"Location: {vm.location}")
+            add_log("-------------------------------")
+    except Exception as e:
+        print(e)
     activeScanning[0] = False
